61760a0036f60268ced44db7d01cf597cbe9c399
Arman Simonyan <arman@qustomerz.com>
2026-02-25 17:36:17 +0400

    created notification

--- src/main.ts
+++ src/main.ts
@@ -37,8 +37,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const port = process.env.PORT || 8080;
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`Swagger documentation: http://localhost:${port}/api`);
 }
